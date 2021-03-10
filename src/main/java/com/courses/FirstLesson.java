785168e8758a5b0fafaea48b556f1e7c7d547c12
Aleksey Danchenko <aleksey.danchenko@gmail.com>
2021-03-10 13:20:01 +0200

    Version_10.03_1

--- src/main/java/com/courses/FirstLesson.java
+++ src/main/java/com/courses/FirstLesson.java
@@ -1,13 +1,33 @@
 package com.courses;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import javax.naming.Name;
 
 public class FirstLesson {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver.exe");
-
+        //System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver.exe");
+        WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
+        driver.get ("https://www.saucedemo.com");
 
-        driver.get ("http://Google.com");
+
+
+        WebElement usernameField = driver.findElement (By.xpath("//input[@id='user-name']"));
+        WebElement passwordField = driver.findElement(By.xpath("//input[2]"));
+        WebElement loginbutton = driver.findElement(By.xpath("//input[@type='submit']"));
+
+        WebElement name = driver.findElement(By.xpath("//div [@id='login_credentials']/text()[last()]"));
+        WebElement pass = driver.findElement(By.xpath("//div [@class='login_password']/text()"));
+
+        usernameField.sendKeys(name.getText());
+        passwordField.sendKeys(pass.getText());
+        loginbutton.click();
+
+
+
     }
 }
