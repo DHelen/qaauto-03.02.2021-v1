bcd7133876d3901d75df39f6d75d701c0b196645
Aleksey Danchenko <aleksey.danchenko@gmail.com>
2021-03-11 09:47:05 +0200

    Version_11.03_1

--- src/main/java/com/courses/FirstLesson.java
+++ src/main/java/com/courses/FirstLesson.java
@@ -4,8 +4,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import javax.naming.Name;
+import java.util.List;
 
 public class FirstLesson {
     public static void main(String[] args) {
@@ -20,14 +24,41 @@ public class FirstLesson {
         WebElement passwordField = driver.findElement(By.xpath("//input[2]"));
         WebElement loginbutton = driver.findElement(By.xpath("//input[@type='submit']"));
 
-        WebElement name = driver.findElement(By.xpath("//div [@id='login_credentials']/text()[last()]"));
-        WebElement pass = driver.findElement(By.xpath("//div [@class='login_password']/text()"));
 
-        usernameField.sendKeys(name.getText());
-        passwordField.sendKeys(pass.getText());
+        String textname = driver.findElement(By.id("login_credentials")).getText();
+        String [] name = textname.split("\n");
+        usernameField.sendKeys(name [1]);
+
+        String textpass = driver.findElement(By.xpath("//div[@class='login_password']")).getText();
+        String [] pass = textpass.split("\n");
+        passwordField.sendKeys( pass [1]);
+
         loginbutton.click();
+
+        List<WebElement> list = driver.findElements(By.xpath("//button[@class='btn_primary btn_inventory']"));
+        WebElement addToCart = list.get(2);
+        addToCart.click();
+
+        WebElement filter = driver.findElement(By.xpath("//select[@class ='product_sort_container']" ));
+        Select select = new Select(filter);
+        List<WebElement> listOfOptions = select.getOptions();
+        select.selectByValue("za");
+
+        List<WebElement> listsecond = driver.findElements(By.xpath("//button[@class='btn_primary btn_inventory']"));
+        WebElement addToCartsecond = listsecond.get(3);
+        addToCartsecond.click();
+
+        WebElement menu = driver.findElement(By.xpath("//button[@id ='react-burger-menu-btn']"));
+        menu.click();
+
+        WebElement logout = driver.findElement(By.xpath("//a[@id='logout_sidebar_link']"));
+        WebDriverWait wait= new WebDriverWait(driver, 100000);
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@id='logout_sidebar_link']")));
+
+        logout.click();
 
 
 
     }
+
 }
